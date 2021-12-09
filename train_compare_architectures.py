b11a6feb652ed19b290da1474778673bb5dcd1ee
Burak Han Demirbilek <burakhan.demirbilek@gmail.com>
2021-12-09 15:13:26 +0000

    Updated dockerfile

--- train_compare_architectures.py
+++ train_compare_architectures.py
@@ -14,6 +14,8 @@ if __name__ == '__main__':
         from parameters_c_architecture import start_no_memory_intr
         from parameters_c_architecture import start_o_k_memory
         from parameters_c_architecture import start_o_k_intr_memory
+        from parameters_c_architecture import start_oa_k_memory
+        from parameters_c_architecture import start_oa_k_intr_memory
         from parameters_c_architecture import start_lstm
         from parameters_c_architecture import start_lstm_intr
 
@@ -21,6 +23,8 @@ if __name__ == '__main__':
         from parameters_c_architecture import no_memory_intr_learning_setting
         from parameters_c_architecture import o_k_memory_learning_setting
         from parameters_c_architecture import o_k_intr_memory_learning_setting
+        from parameters_c_architecture import oa_k_memory_learning_setting
+        from parameters_c_architecture import oa_k_intr_memory_learning_setting
         from parameters_c_architecture import lstm_learning_setting
         from parameters_c_architecture import lstm_intr_learning_setting
 
@@ -54,6 +58,20 @@ if __name__ == '__main__':
             p = mp.Process(
                 target=o_k_intr_memory_learning_setting['train_func'],
                 kwargs={'learning_setting': o_k_intr_memory_learning_setting})
+            p.start()
+            processes.append(p)
+
+        if start_oa_k_memory:
+            p = mp.Process(
+                target=o_k_memory_learning_setting['train_func'],
+                kwargs={'learning_setting': oa_k_memory_learning_setting})
+            p.start()
+            processes.append(p)
+
+        if start_oa_k_intr_memory:
+            p = mp.Process(
+                target=o_k_intr_memory_learning_setting['train_func'],
+                kwargs={'learning_setting': oa_k_intr_memory_learning_setting})
             p.start()
             processes.append(p)
 
