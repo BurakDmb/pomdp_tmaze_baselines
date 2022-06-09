af27027cf7bd91c2f7623d20dfd75bfa56140900
Burak Han Demirbilek <burakhan.demirbilek@gmail.com>
2022-06-09 17:19:42 +0300

    Updated AE code

--- pomdp_tmaze_baselines/utils/AE.py
+++ pomdp_tmaze_baselines/utils/AE.py
@@ -20,9 +20,13 @@ class Autoencoder(nn.Module):
         self.maxpool_stride = maxpool_stride
 
         self.encoder = Encoder(
-            input_dims, latent_dims, hidden_size, in_channels)
+            input_dims, latent_dims, hidden_size, in_channels,
+            kernel_size, padding, dilation,
+            conv_hidden_size, conv1_stride, maxpool_stride)
         self.decoder = Decoder(
-            input_dims, latent_dims, hidden_size, in_channels)
+            input_dims, latent_dims, hidden_size, in_channels,
+            kernel_size, padding, dilation,
+            conv_hidden_size, conv1_stride, maxpool_stride)
         # self.z = torch.empty((batch_size, latent_dims))
 
     def forward(self, x):
@@ -106,3 +110,78 @@ class Decoder(nn.Module):
             -1, self.conv_hidden_size,
             self.decoder_h_in, self.decoder_h_in))
         return self.conv1(z)
+
+
+class VariationalAutoencoder(nn.Module):
+    def __init__(
+            self, input_dims, latent_dims, hidden_size, in_channels,
+            kernel_size=3, padding=3, dilation=1,
+            conv_hidden_size=16, conv1_stride=4, maxpool_stride=1):
+        super(VariationalAutoencoder, self).__init__()
+
+        self.kernel_size = kernel_size
+        self.padding = padding
+        self.dilation = dilation
+        self.conv_hidden_size = conv_hidden_size
+        self.conv1_stride = conv1_stride
+        self.maxpool_stride = maxpool_stride
+
+        self.encoder = VariationalEncoder(
+            input_dims, latent_dims, hidden_size, in_channels)
+        self.decoder = Decoder(
+            input_dims, latent_dims, hidden_size, in_channels)
+        # self.z = torch.empty((batch_size, latent_dims))
+
+    def forward(self, x):
+        self.z = self.encoder(x)
+        return self.decoder(self.z)
+
+
+class VariationalEncoder(nn.Module):
+    def __init__(
+            self, input_dims, latent_dims, hidden_size, in_channels,
+            kernel_size, padding, dilation,
+            conv_hidden_size, conv1_stride, maxpool_stride):
+        super(VariationalEncoder, self).__init__()
+        self.input_dims = input_dims
+
+        # First Conv Layer
+        h_out1 = int(np.floor(((
+            input_dims + 2*padding - dilation * (kernel_size-1) - 1)
+            / conv1_stride) + 1))
+
+        self.conv1 = nn.Conv2d(
+            in_channels=in_channels,
+            out_channels=conv_hidden_size,
+            kernel_size=kernel_size,
+            stride=conv1_stride,
+            padding=padding,
+            dilation=dilation)
+        self.relu1 = nn.ReLU()
+
+        self.maxpool1 = nn.MaxPool2d(
+            kernel_size=kernel_size, stride=maxpool_stride)
+        h_out2 = int(np.floor(((
+            h_out1 - dilation * (kernel_size-1) - 1)
+            / maxpool_stride) + 1))
+
+        self.linear1 = nn.Linear(h_out2*h_out2*conv_hidden_size, hidden_size)
+        self.linear2 = nn.Linear(hidden_size, latent_dims)
+        self.linear3 = nn.Linear(hidden_size, latent_dims)
+
+        self.N = torch.distributions.Normal(0, 1)
+        self.N.loc = self.N.loc.cuda()  # hack to get sampling on the GPU
+        self.N.scale = self.N.scale.cuda()
+        self.kl = 0
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.relu1(x)
+        x = self.maxpool1(x)
+        x = torch.flatten(x, start_dim=1)
+        x = F.relu(self.linear1(x))
+        mu = self.linear2(x)
+        sigma = torch.exp(self.linear3(x))
+        z = mu + sigma*self.N.sample(mu.shape)
+        self.kl = (sigma**2 + mu**2 - torch.log(sigma) - 1/2).sum()
+        return z
