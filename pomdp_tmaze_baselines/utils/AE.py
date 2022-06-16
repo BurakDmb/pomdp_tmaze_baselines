660c38c5f2d3b2213e8827601473181e27e19531
Burak Han Demirbilek <burakhan.demirbilek@gmail.com>
2022-06-16 20:57:22 +0300

    Updated code

--- pomdp_tmaze_baselines/utils/AE.py
+++ pomdp_tmaze_baselines/utils/AE.py
@@ -3,6 +3,133 @@ import numpy as np
 import torch
 from torch import nn
 import torch.nn.functional as F
+from torchsummary import summary
+
+
+# k: kernel_size, p: padding, s: stride, d: dilation.
+def conv_shape(x, k=1, p=0, s=1, d=1):
+    return int((x + 2*p - d*(k - 1) - 1)/s + 1)
+
+
+class ConvAutoencoder(nn.Module):
+    def __init__(
+            self, input_dims, latent_dims, hidden_size, in_channels,
+            kernel_size=3, padding=3, dilation=1,
+            conv_hidden_size=16, conv1_stride=4, maxpool_stride=1):
+        super(ConvAutoencoder, self).__init__()
+
+        self.kernel_size = kernel_size
+        self.padding = padding
+        self.dilation = dilation
+        self.conv_hidden_size = conv_hidden_size
+        self.conv1_stride = conv1_stride
+        self.maxpool_stride = maxpool_stride
+
+        self.encoder = EncoderConv(
+            input_dims, latent_dims, hidden_size, in_channels,
+            kernel_size, padding, dilation,
+            conv_hidden_size, conv1_stride, maxpool_stride)
+        self.decoder = DecoderConv(
+            input_dims, latent_dims, hidden_size, in_channels,
+            kernel_size, padding, dilation,
+            conv_hidden_size, conv1_stride, maxpool_stride)
+        # self.z = torch.empty((batch_size, latent_dims))
+        summary(self.to("cuda"), (3, 48, 48))
+
+    def forward(self, x):
+        self.z = self.encoder(x)
+        result = self.decoder(self.z)
+        return result
+
+
+# Source: https://avandekleut.github.io/vae/
+class EncoderConv(nn.Module):
+    def __init__(
+            self, input_dims, latent_dims, hidden_size, in_channels,
+            kernel_size, padding, dilation,
+            conv_hidden_size, conv1_stride, maxpool_stride):
+        super(EncoderConv, self).__init__()
+        self.input_dims = input_dims
+
+        self.shape2d_1 = conv_shape(self.input_dims, k=kernel_size)
+        self.shape2d_2 = conv_shape(self.shape2d_1, k=kernel_size)
+        self.shape2d_3 = conv_shape(self.shape2d_2, k=kernel_size)
+
+        self.shape1d_1 = conv_shape(self.shape2d_3, k=kernel_size)
+        self.shape1d_2 = conv_shape(self.shape1d_1, k=kernel_size)
+        self.shape1d_3 = conv_shape(self.shape1d_2, k=kernel_size)
+
+        # encoder
+        self.enc2d_1 = nn.Conv2d(3, 32, kernel_size)
+        self.enc2d_2 = nn.Conv2d(32, 32, kernel_size)
+        self.enc2d_3 = nn.Conv2d(32, 1, kernel_size)
+
+        self.enc1d_1 = nn.Conv1d(self.shape2d_3, 32, kernel_size)
+        self.enc1d_2 = nn.Conv1d(32, 32, kernel_size)
+        self.enc1d_3 = nn.Conv1d(32, 8, kernel_size)
+
+        self.linear1 = nn.Linear(8*self.shape1d_3, hidden_size)
+        self.linear2 = nn.Linear(hidden_size, latent_dims)
+
+    def forward(self, x):
+        x = F.relu(self.enc2d_1(x))
+        x = F.relu(self.enc2d_2(x))
+        x = F.relu(self.enc2d_3(x))
+        x = x.flatten(start_dim=1, end_dim=2)
+        x = F.relu(self.enc1d_1(x))
+        x = F.relu(self.enc1d_2(x))
+        x = F.relu(self.enc1d_3(x))
+        x = x.flatten(start_dim=1, end_dim=2)
+        x = F.relu(self.linear1(x))
+        x = F.relu(self.linear2(x))
+        return x
+
+
+class DecoderConv(nn.Module):
+    def __init__(
+            self, input_dims, latent_dims, hidden_size, in_channels,
+            kernel_size, padding, dilation,
+            conv_hidden_size, conv1_stride, maxpool_stride):
+        super(DecoderConv, self).__init__()
+        self.input_dims = input_dims
+        self.hidden_size = hidden_size
+
+        self.conv_hidden_size = conv_hidden_size
+
+        ####
+        self.shape2d_1 = conv_shape(self.input_dims, k=kernel_size)
+        self.shape2d_2 = conv_shape(self.shape2d_1, k=kernel_size)
+        self.shape2d_3 = conv_shape(self.shape2d_2, k=kernel_size)
+
+        self.shape1d_1 = conv_shape(self.shape2d_3, k=kernel_size)
+        self.shape1d_2 = conv_shape(self.shape1d_1, k=kernel_size)
+        self.shape1d_3 = conv_shape(self.shape1d_2, k=kernel_size)
+
+        # decode
+        self.linear1 = nn.Linear(latent_dims, hidden_size)
+        self.linear2 = nn.Linear(hidden_size, 8*self.shape1d_3)
+
+        self.dec1d_1 = nn.ConvTranspose1d(8, 32, kernel_size)
+        self.dec1d_2 = nn.ConvTranspose1d(32, 32, kernel_size)
+        self.dec1d_3 = nn.ConvTranspose1d(32, self.shape2d_3, kernel_size)
+
+        self.dec2d_1 = nn.ConvTranspose2d(1, 32, kernel_size)
+        self.dec2d_2 = nn.ConvTranspose2d(32, 32, kernel_size)
+        self.dec2d_3 = nn.ConvTranspose2d(32, 3, kernel_size)
+
+    def forward(self, z):
+
+        z = F.relu(self.linear1(z))
+        z = F.relu(self.linear2(z))
+        z = z.unflatten(1, (8, self.shape1d_3))
+        z = F.relu(self.dec1d_1(z))
+        z = F.relu(self.dec1d_2(z))
+        z = F.relu(self.dec1d_3(z))
+        z = z.unflatten(1, (1, self.shape2d_3))
+        z = F.relu(self.dec2d_1(z))
+        z = F.relu(self.dec2d_2(z))
+        z = F.relu(self.dec2d_3(z))
+        return z
 
 
 class Autoencoder(nn.Module):
@@ -44,9 +171,9 @@ class Encoder(nn.Module):
         self.input_dims = input_dims
 
         # First Conv Layer
-        h_out1 = int(np.floor(((
-            input_dims + 2*padding - dilation * (kernel_size-1) - 1)
-            / conv1_stride) + 1))
+        h_out1 = conv_shape(
+            input_dims, k=kernel_size,
+            p=padding, s=conv1_stride, d=dilation)
 
         self.conv1 = nn.Conv2d(
             in_channels=in_channels,
@@ -59,9 +186,8 @@ class Encoder(nn.Module):
 
         self.maxpool1 = nn.MaxPool2d(
             kernel_size=kernel_size, stride=maxpool_stride)
-        h_out2 = int(np.floor(((
-            h_out1 - dilation * (kernel_size-1) - 1)
-            / maxpool_stride) + 1))
+        h_out2 = conv_shape(
+            h_out1, k=kernel_size, s=maxpool_stride, d=dilation)
 
         # Linear Layers
         self.linear1 = nn.Linear(h_out2*h_out2*conv_hidden_size, hidden_size)
